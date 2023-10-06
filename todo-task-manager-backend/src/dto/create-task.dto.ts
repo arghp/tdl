d9d4c29399819919d02a8b0465ed35f00b5aeaae
Arpan Gurung <arpan.gurung@zoominfo.com>
2023-10-06 18:53:00 -0400

    feat: update task schema

--- todo-task-manager-backend/src/dto/create-task.dto.ts
+++ todo-task-manager-backend/src/dto/create-task.dto.ts
@@ -1,6 +1,11 @@
+import { Priority } from "src/models/priority.enum";
+import { Status } from "src/models/status.enum";
+
 export class CreateTaskDto {
     readonly title: string;
     readonly description: string;
-    readonly status: string;
+    readonly status: Status;
     readonly category: string;
+    readonly priority: Priority;
+    readonly dueDate: Date;
 }
